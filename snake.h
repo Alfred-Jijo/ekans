fb2b5c8a3996d18d0c5779e4b015c91b3270a74a
Alfred Jijo <alfredjijo28@gmail.com>
2025-05-31 20:54:59 +0000

    May 31, 2025, 9:54 PM

--- snake.h
+++ snake.h
@@ -1,8 +1,16 @@
-#ifndef SNAKE_H_
+#ifndef SNAKE_H
 #define SNAKE_H_
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <stdbool.h>
+
+enum {
+	GRID_WIDTH = 30,
+	GRID_HEIGHT = 20,
+	CELL_SIZE = 20,
+	MAX_SNEK_LENGTH = (GRID_WIDTH * GRID_HEIGHT) - 1,
+	INITIAL_SNEK_LENGTH = 5,
+};
 
 #endif // SNAKE_H_
