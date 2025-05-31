5968042449565fecb87a6c936c755a7c6bc18107
Alfred Jijo <alfredjijo28@gmail.com>
2025-05-31 21:01:46 +0000

    May 31, 2025, 10:01 PM

---
+++ game.c
@@ -0,0 +1,24 @@
+#include "snake.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+GameState game_state = {
+    .snake_length = INITIAL_SNEK_LENGTH,
+    .current_direction = DIR_RIGHT,
+    .input_direction = DIR_RIGHT,
+    .game_over = false,
+    .score = 0,
+    .timer_id = 0,
+    .game_speed = GAME_SPEED
+};
+
+void 
+init_game (void)
+{
+        int start_x = GRID_WIDTH / 4;
+        int start_y = GRID_HEIGHT / 2;
+        for (int i = 0; i < game_state.snake_length; ++i) {
+            game_state.snake[i].x = start_x - i;
+            game_state.snake[i].y = start_y;
+        }
+}
