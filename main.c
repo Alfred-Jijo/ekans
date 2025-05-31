ff13af707fbb67b133b3db7d0c4d0653597a12bb
Alfred Jijo <alfredjijo28@gmail.com>
2025-05-31 21:33:41 +0000

    May 31, 2025, 10:33 PM

--- main.c
+++ main.c
@@ -25,3 +25,18 @@ WinMain(
         MSG msg;
         WNDCLASSEX wc = {0};
 }
+
+LRESULT CALLBACK
+WindowProc (
+        HWND hwnd,
+        UINT msg,
+        WPARAM wParam,
+        LPARAM lParam)
+{
+        switch (msg)
+        {
+                default: return DefWindowProc(hwnd, msg, wParam, lParam);
+        }
+
+        return 0;
+}
