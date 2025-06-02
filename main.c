3d34a4d6b70d8ba92a8a3eb79706153c89c53994
Alfred Jijo <alfredjijo28@gmail.com>
2025-06-02 19:29:38 +0000

    Jun 2, 2025, 8:29 PM

--- main.c
+++ main.c
@@ -22,7 +22,61 @@ int WinMain(
 
 	HWND hwnd;
 	MSG msg;
-	WNDCLASSEX wc = {0};
+	WNDCLASSEX wc = {
+                .cbSize = sizeof(WNDCLASSEX),
+                .style = CS_HREDRAW | CS_VREDRAW,
+                .lpfnWndProc = WindowProc,
+                .cbClsExtra = 0,
+                .cbWndExtra = 0,
+                .hInstance = hInstance,
+                .hIcon = LoadIcon(NULL, IDI_APPLICATION),
+                .hCursor = LoadCursor(NULL, IDC_ARROW),
+                .hbrBackground = (HBRUSH)(COLOR_WINDOW + 1),
+                .lpszMenuName = NULL,
+                .lpszClassName = L"Ekans",
+                .hIconSm = LoadIcon(NULL, IDI_APPLICATION)
+        };
+
+        if (!RegisterClassEx(&wc)) {
+                MessageBoxW(NULL, L"Failed to register window class!",
+                         L"Error", MB_ICONEXCLAMATION | MB_OK);
+                return 1;
+        }
+
+        RECT wndRect = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
+        AdjustWindowRect(&wndRect, WS_OVERLAPPEDWINDOW, FALSE);
+        int adjustedWidth = wndRect.right - wndRect.left;
+        int adjustedHeight = wndRect.bottom - wndRect.top;
+
+        hwnd = CreateWindowEx(
+                WS_EX_CLIENTEDGE,
+                L"Ekans", 
+                L"Ekans - Snake Game",
+                WS_OVERLAPPEDWINDOW, & ~WS_THICKFRAME, & ~WS_MAXIMIZEBOX,
+                CW_USEDEFAULT, CW_USEDEFAULT,
+                adjustedWidth, adjustedHeight,
+                NULL, NULL, hInstance, NULL 
+        );
+
+        if (!hwnd) {
+                MessageBoxW(NULL, L"Failed to create window!",
+                         L"Error", MB_ICONEXCLAMATION | MB_OK);
+                return 1;
+        }
+
+        game_state.hwnd_main = hwnd;
+        show_window(hwnd, nCmdShow);
+        update_window(hwnd);
+
+        srand((unsigned int)time(NULL)); 
+        init_game();
+
+        while (GetMessage(&msg, NULL, 0, 0) > 0) {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+        }
+
+        return (int)msg.wParam;
 }
 
 LRESULT CALLBACK
@@ -35,14 +89,35 @@ WindowProc(
 	switch (msg)
 	{
 	case WM_CREATE:
+                if (game_state.timer_id == 0 && game_state.hwnd_main != NULL) {
+                        game_state.timer_id = SetTimer(hwnd, GAME_TIMER_ID, game_state.game_speed, NULL);
+                        if (game_state.timer_id == 0) {
+                                MessageBoxW(hwnd, L"Couldnt Set Timer", L"Error", MB_OK | MB_ICONERROR);
+                                PostQuitMessage(0);
+                        }
+                }
 		break;
 	case WM_TIMER:
+                if (wParam == game_state.timer_id) {
+                        if (!game_state.game_over) {
+                                update_game();
+                                InvalidateRect(hwnd, NULL, TRUE);
+                        }
+                }
 		break;
 	case WM_PAINT:
-		break;
+                {
+                        PAINTSTRUCT ps;
+                        HDC hdc = BeginPaint(hwnd, &ps);
+                        draw_game(hdc);
+                        EndPaint(hwnd, &ps);
+                }
+                break;
 	case WM_KEYDOWN:
 		handle_input(game_state.input_direction, wParam);
 		break;
+                case WM_ERASEBKGND:
+                        return 1; // Prevent flickering by not erasing background
 	case WM_DESTROY:
 		if (game_state.timer_id != 0)
 		{
@@ -51,8 +126,6 @@ WindowProc(
 		}
 		PostQuitMessage(0);
 		break;
-	case WM_ERASEBKGND:
-		return 1; // Prevent flickering by not erasing background
 	default:
 		return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
